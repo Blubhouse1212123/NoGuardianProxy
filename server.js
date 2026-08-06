350d6756d289da2a25151d614ef19df2851c69e9
Blubhouse1212123 <rdw196250@gmail.com>
2026-08-06 15:57:46 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Proxy Online!");
+});
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server Started!");
+});
