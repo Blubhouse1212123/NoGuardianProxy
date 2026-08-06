d39a01b6db8df956c8c8e30dad732587091ddc27
Blubhouse1212123 <rdw196250@gmail.com>
2026-08-06 16:34:06 -0400

    Added browse endpoint

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Proxy Online!");
 });
+app.post("/browse", async (req, res) => {
+    try {
+        const url = req.body.url;
+        if (!url) {
+            return res.status(400).send("Missing Url");
+        }
+        const response = await fetch(url);
+        const html = await response.text();
+        res.send(html);
+    } catch(error) {
+        console.error(error);
+        res.status(500).send(
+            "Failed to fetch website"
+        );
+    }
+});
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server Started!");
 });
